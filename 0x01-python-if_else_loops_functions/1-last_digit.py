66dc592651db323ee1aff076ee4d919389669fa6
cgrade <gradeisageek@gmail.com>
2022-11-29 07:48:48 -0800

    ValueErr instead of TypeErr

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,13 +3,13 @@ import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
 lastD = str(number)[-1]
-lastD = int(lastD)
 if number < 0:
-    lastD = -lastD
-if lastD > 5:
+    lastD = -int(lastD)
+if int(lastD) > 5:
     print(f"Last digit of {number:d} is {lastD:d} and is greater than 5")
-elif lastD == 0:
+elif int(lastD) == 0:
     print(f"Last digit of {number:d} is {lastD:d} and is {lastD:d}")
-elif lastD < 6:
-    if lastD != 0:
+elif int(lastD) < 6:
+    if int(lastD) != 0:
+        lastD = int(lastD)
         print(f"Last digit of {number:d} is {lastD:d} and is less than 6 and not 0")
