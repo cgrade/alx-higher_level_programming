c0fed22836930df5986354250a27addb3a734eff
cgrade <gradeisageek@gmail.com>
2022-11-29 07:26:52 -0800

    semantic error fixed

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,11 +4,13 @@ number = random.randint(-10000, 10000)
 # YOUR CODE HERE
 lastD = str(number)[-1]
 lastD = int(lastD)
+if number < 0:
+    lastD = -lastD
 if lastD > 5:
     print(f"Last digit of {number:d} is {lastD:d} and is greater than 5")
 elif lastD == 0:
     print(f"Last digit of {number:d} is {lastD:d} and is zero")
 elif lastD < 6:
-    if lastD != 0:
+  if lastD != 0:
         print(f"Last digit of {number:d} is {lastD:d}  and is less than 6 and not 0")
 
