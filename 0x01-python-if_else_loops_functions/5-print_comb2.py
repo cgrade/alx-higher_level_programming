abd3551b7c65967fc3efcc0ccd9c427e6b9e504b
cgrade <gradeisageek@gmail.com>
2022-11-29 21:38:41 -0800

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for num in range(100):
-    print("{:02d}, ".format(num), end="")
+for num in range(99):
+    print("{:02d},".format(num), end=" ")
+print("{:02d}".format(num + 1))
