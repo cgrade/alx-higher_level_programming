115122cc0ff01c7766df9206dfe9b88c8d42127b
cgrade <gradeisageek@gmail.com>
2022-12-09 00:06:54 -0800

    task 5 added

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
     count = 0
-    for key, value in a_dictionary.items():
-        count =+ 1
+    for key in a_dictionary.keys():
+        count += 1
     return (count)
