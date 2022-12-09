9c7c690dad39b27a578c4dcbda4e721324f243d9
cgrade <gradeisageek@gmail.com>
2022-12-09 00:58:38 -0800

    task added

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    key_list = list()
+    new_dict = dict()
+
+    for key in a_dictionary.keys():
+        key_list.append(key)
+    sortd_keylist = sorted(key_list)
+    for item in sortd_keylist:
+        new_dict[item] = a_dictionary[item]
+    #print(new_dict)
+    for keys, values in new_dict.items():
+        print("{}: {}".format(keys, values))
