dc7ca17634c568ca6c3949ae6781164e8e0b0ed0
cgrade <gradeisageek@gmail.com>
2022-12-20 20:36:10 -0800

    bug fixed

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,5 +4,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return(True)
     except Exception as e:
-        print(e)
+        print("Exception: {}".format(e))
         return(False)
