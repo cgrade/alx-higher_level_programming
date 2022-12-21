8d4007a0868fce160db74d122e75a94c271af1df
cgrade <gradeisageek@gmail.com>
2022-12-20 16:19:24 -0800

    bug fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
     n = 0
-    for i in range(x):
+    for m in range(x):
         try:
-            print(my_list[i], end='')
-            n =+ 1
-        except Exception:
+            print(my_list[m], end="")
+            n += 1
+        except IndexError:
             break
     print()
-    return(x)
+    return n
