f4b7a077c067b91d486c079a635368c76276999c
cgrade <gradeisageek@gmail.com>
2022-12-20 16:08:09 -0800

    task 1 added

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-    for i in range(1, x+1):
-        print(i, end='')
-    print()
+    try:
+        for i in range(1, x+1):
+            print(i, end='')
+        print()
+        
+    except:
+        pass
     return(x)
 
