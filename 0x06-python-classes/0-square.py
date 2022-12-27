c2ac2bab2d250c8acb6f1d452d19c177cc491a51
cgrade <gradeisageek@gmail.com>
2022-12-26 17:47:34 -0800

    docstrings fixed

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""This is a File that Describes
+how a simple class is created"""
+
 class Square:
-    """This Classes Defines a Squre"""
+    """This Classes Defines a Square"""
     pass
