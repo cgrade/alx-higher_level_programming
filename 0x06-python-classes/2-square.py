d4a3234ab1eea78de4a31b5448424a7ba79f4fab
cgrade <gradeisageek@gmail.com>
2022-12-26 19:09:38 -0800

    task 2 added

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -12,11 +12,9 @@ class Square:
     """
     def __init__(self, size=0):
         """Constructor of the Square Classe"""
-        try:
-            size == isinstance(int)
-        except TypeError:
-            print('size must be an integer')
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
 
-        if int(size) < 0:
+        elif size < 0:
             raise ValueError('size must be >= 0')
         self.__size = size
