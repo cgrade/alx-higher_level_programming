e276981f0515cbeabddf9cef30d5c8e6b0fd85bf
cgrade <gradeisageek@gmail.com>
2022-12-26 19:49:03 -0800

    task added

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+A script that Defines a Class,
+private instance attributes
+and optional instantiations
+"""
+
+
+class Square:
+    """ A Class that computes
+    The Square of an int(obj)
+    """
+    def __init__(self, size=0):
+        """Constructor of the Square Classe"""
+        self.size = size
+
+    def area(self):
+        """ compute the Area of the size"""
+        return (self.__size**2)
+
+    def my_print(self):
+        """Print to stdout"""
+        if self.size == 0:
+            return()
+        else:
+            sh = "#" * size
+            return(sh ** sh)
+
+    @property
+    def size(self):
+        """getter"""
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
