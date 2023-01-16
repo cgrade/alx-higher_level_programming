dba0e2b2b4a7ece4918a47f6ee05904dd4136ec5
cgrade <gradeisageek@gmail.com>
2023-01-16 15:44:39 -0800

    tasks addded

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,10 +8,7 @@ and all the elements must be and int.
 
 
 class Mylist(list):
-    """
-    A class that Inherit the builtins list class
-    Hence: it's called a subclass of list
-    """
+    """A class that Inherit from list"""
 
     def print_sorted(self):
         """ A method that sort the elements of the list"""
