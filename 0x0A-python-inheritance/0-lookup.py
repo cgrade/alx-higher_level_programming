9b6ed133175a8cbf8a359f4fa56132b1695d24c5
cgrade <gradeisageek@gmail.com>
2023-01-16 15:07:46 -0800

    task 0 added

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+This module contains a function with the prototype:
+def lookup(object)
+and returns a list of the attributes of the object.
+"""
+
+def lookup(obj):
+    """ A function that returns a list of attributes
+    of an object 
+    @param: obj --> Object to lookup it's attributes
+    Returns: List
+    """
+    return (dir(obj))
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,3 @@
+# Python inheritance Project
+
+## This Repository contains all the tasks i did during the course of learning Python inheritace
