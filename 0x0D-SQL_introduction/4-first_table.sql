40235a85e851fa711e15e7beccc26a7f23a74e13
cgrade <gradeisageek@gmail.com>
2023-02-15 00:50:48 +0100

    Task 4: Bug fix

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- A script that creates a table called "First_table"
-CREATE TABLE IF NOT EXISTS first_table(id(INT), name(VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
