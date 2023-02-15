553d763c67ea6f43b1aedf001a3ef183e74d8c9b
cgrade <gradeisageek@gmail.com>
2023-02-15 13:48:05 +0100

    task 0 and REadme file added

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- A script that list all Privileges of Mysql users:
+
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,5 @@
+# This is the Readme for SQL 
+
+## More Querries were made in this practice
+
+### This reamde will be updated soon.
