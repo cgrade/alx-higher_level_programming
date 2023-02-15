32c394e497533fd1e3158d5560d7bea9d1e6130b
cgrade <gradeisageek@gmail.com>
2023-02-15 15:12:01 +0100

    Task 6: A Sql script that Creates a Database, Create a Table inside the Database, apply Constraints, and Primary Key to the fields

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- A script that crates a database, table and PK
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY, name VARCHAR(256) NOT NULL);
