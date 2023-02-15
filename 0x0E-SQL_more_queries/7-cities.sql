1ec14b9605b10cd7500b65e5ac627af70e0c5852
cgrade <gradeisageek@gmail.com>
2023-02-15 15:24:00 +0100

    Task 7: A sql script that creates a database, creates a tabale and asign PK FK to fields in the Table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,5 +1,4 @@
---A sql script that create database and tables with few tweaking constraints
+-- creates database hbtn_0d_usa and cities table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY, state_id INT NOT NULL FOREIGN KEY(state_id) REFRENCES states(id), name VARCHAR(256) NOT NULL);
-
+CREATE TABLE IF NOT EXISTS cities(id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES states(id), name VARCHAR(256) NOT NULL);
