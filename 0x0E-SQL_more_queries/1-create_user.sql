50db31d4545ab0fb992bc176868136b5dc6fd0ca
cgrade <gradeisageek@gmail.com>
2023-02-15 14:06:20 +0100

    task 1: bug fix

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- A script that creates a user and assign privileges;
 
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_pwd';
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON mysql.database TO user_0d_1@localhost;
