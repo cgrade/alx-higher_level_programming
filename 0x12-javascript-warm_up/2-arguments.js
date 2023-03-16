9212a8065da68750cd589d9e1871f6106dace1d4
cgrade <gradeisageek@gmail.com>
2023-03-16 21:00:55 +0100

    js tasks

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const argc = process.argv.length;i
+const argc = process.argv.length; i;
 if (argc > 2) {
-	console.log('Argument' + (argc > 3 ? 's' : '') + ' found');
+  console.log('Argument' + (argc > 3 ? 's' : '') + ' found');
 } else {
-	console.log('No argument');
+  console.log('No argument');
 }
