4be9756b039711601caaa485918b5e7c40309a3d
cgrade <gradeisageek@gmail.com>
2023-03-20 08:03:46 -0700

    model_state script created

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -8,12 +8,11 @@ Base Class instantiation
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-if __name__ == "__main__":
-    """Base Class instatiation"""
-    Base = declarative_base()
+Base = declarative_base()
 
-    class States(Base):
-        """State Class model, that maps to the `state` table """
-        __tablename__ = 'states'
-        id = Column(Integer, primary_key=True)
-        nname = Column(String(128), nullable=False)
+
+class State(Base):
+    """State Class model, that maps to the `state` table """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
