b6521f32787a3c8d29b6ffd8a87cabc65fac4e42
cgrade <gradeisageek@gmail.com>
2023-03-20 05:58:28 -0700

    bug fixed

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -4,15 +4,20 @@ A script that lists all rows form the
 citiees table of `hbtn_0e_4_usa` database
 """
 
-#check if module is running on `main`
+"""check if module is running on `main`"""
+
 if __name__ == '__main__':
-    
-    #import necesary dependencies
+    """import necesary dependencies"""
+
     import MySQLdb as db
     from sys import argv
 
-    db_connector = db.connect(host="localhost",
-            port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    db_connector = db.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3])
     db_cursor = db_connector.cursor()
     db_cursor.execute("""SELECT cities.id, cities.name, states.name \
             FROM cities JOIN states ON cities.state_id = states.id \
