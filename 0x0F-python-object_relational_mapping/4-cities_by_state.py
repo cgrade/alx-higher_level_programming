5c1655f9e44757d9efde105d0e486d5aa140c0c6
cgrade <gradeisageek@gmail.com>
2023-03-20 05:50:05 -0700

    corrections made

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -14,7 +14,9 @@ if __name__ == '__main__':
     db_connector = db.connect(host="localhost",
             port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     db_cursor = db_connector.cursor()
-    db_cursor.execute("""SELECT * FROM cities ORDER BY cities.id ASC""")
+    db_cursor.execute("""SELECT cities.id, cities.name, states.name \
+            FROM cities JOIN states ON cities.state_id = states.id \
+            ORDER BY cities.id ASC""")
     row_fetched = db_cursor.fetchall()
     for row in row_fetched:
         print(row)
