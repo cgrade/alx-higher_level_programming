9cc121aadeebb08be03f07fd580c20ea84304600
cgrade <gradeisageek@gmail.com>
2023-03-20 07:55:24 -0700

    task 6 added

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+A script that contains 
+state class definiation and 
+Base Class instantiation
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+if __name__ == "__main__":
+    """Base Class instatiation"""
+    Base = declarative_base()
+
+    class States(Base):
+        """State Class model, that maps to the `state` table """
+        __tablename__ = 'states'
+        id = Column(Integer, primary_key=True)
+        nname = Column(String(128), nullable=False)
