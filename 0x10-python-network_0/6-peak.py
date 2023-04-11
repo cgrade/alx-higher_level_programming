d15dfb3aa24048f7f703263fd2a41c6aed13fdd7
cgrade <gradeisageek@gmail.com>
2023-04-11 05:24:03 +0100

    python script that contains a python function that searches for the peak of int in an unsorted list of integers

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Searches for the peak value in an unsroted list of integers"""
+
+def find_peak(list_of_integers):
+    """
+    Args: List of Integers(int): unsorted lists
+    Returns: The peak of the unsorted lists
+    """
+    size_list = len(list_of_integers)
+    mid_val = size_list
+    m = size_list //2
+
+    if size_list == 0:
+        return None
+
+    else:
+        while True:
+            mid_val = mid_val //2
+            if (mid < size_list - 1 && list_of_integerss[m] < list_of_integers[m + 1]):
+                if mid_val // 2 == 0:
+                    mid_val = 2
+                m = mid_val // 2
+            elif mid_val > 0 and list_of_integers[m] < list_of_integers[m - 1]:
+                if mid_val // 2 == 0:
+                    mid_val = 2
+                m = m -mid_val // 2
+            else:
+                return list_of_integers[m]
