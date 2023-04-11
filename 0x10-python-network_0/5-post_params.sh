be75be37fba6923409474d79a73157de883d324a
cgrade <gradeisageek@gmail.com>
2023-04-11 05:04:19 +0100

    Task 5:LA script that uses curl tosend a POST request to the server and displays the response to stdout

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that uses curl to send a POST request to the server and displays the response to stdout
-curl -d "email=test@gmail.com&subject='I will always be here for PLD'" "$1"
+curl -sd "email=test@gmail.com&subject='I will always be here for PLD'" "$1"
