2a3be1644f3907d37a4c75aca61008b0ae05f172
cgrade <gradeisageek@gmail.com>
2023-04-13 15:59:28 +0100

    Task 0: a python script that fectches response from a url using urllib

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" A python script that uses urllib to query a url and return a response
+"""
+import urllib.request
+
+if __name__ == "__main__":
+    # URL to fetch
+    url = "https://alx-intranet.hbtn.io/status"
+
+    # Fetch the URL and store the response
+    with urllib.request.urlopen(url) as response:
+        # Read the response body as bytes
+        response_bytes = response.read()
+
+        print("Body response:")
+        print("\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
+              .format(type(response_bytes), response_bytes, response_bytes.decode('utf-8')))
