8317771b09183f03842ef03c183b0a905faa6378
cgrade <gradeisageek@gmail.com>
2023-04-13 23:12:14 +0100

    Python script that takes in a URL, sends a request to the URL and displays the body of the response

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL, sends
+a request to the URL and displays the body of the response
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    error = requests.get(sys.argv[1])
+    if error.status_code >= 400:
+        print('Error code: {}'.format(error.status_code))
+    else:
+        print(error.text)
+
+
