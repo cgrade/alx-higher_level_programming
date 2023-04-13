3b3c0bac6278ff1f66c5154c411bcfe81816752d
cgrade <gradeisageek@gmail.com>
2023-04-13 17:10:55 +0100

    A script that takes in 2 arguments, a url and an email address, it uses urllib to send a post request with the data(email) passed

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" A script that takes in 2 args, an email address and a url
+    it uses urllib to send a post request to the server
+"""
+
+import sys
+import urllib.request
+import urllib.parse
+
+if __name__ == '__main__':
+
+    url = sys.argv[1]
+    values = {'email': sys.argv[2]}
+
+    data = urllib.parse.urlencode(values).encode
+    req = urllib.request.Request(url, data)
+
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode())
