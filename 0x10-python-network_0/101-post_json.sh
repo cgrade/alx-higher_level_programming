40629db3d29b8420aa1ebc5eca5153296b1f1f49
cgrade <gradeisageek@gmail.com>
2023-04-13 01:08:32 +0100

    Advanced Task: a shell script that sends a json file via the post method of a curl statment

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # A script that sends a json file via POST method in a curl statment
-curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1" 
-
+curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"
